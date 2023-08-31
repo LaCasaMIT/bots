f4793dc8cb5ff9def11f71008563d9ba07605e04
maxzetina <74638275+maxzetina@users.noreply.github.com>
2023-08-31 12:24:19 -0400

    updated for db structure changes

--- interface.py
+++ interface.py
@@ -1,11 +1,8 @@
 import pymysql
 
 class Resident:
-    def __init__(self, fname, lname, kerb, year, onExec, room):
-        self.fname = fname
-        self.lname = lname
+    def __init__(self, kerb, onExec, room):
         self.kerb = kerb
-        self.year = year
         self.previous_pts = 0
         self.room_assignment_pts = 0
         self.chores_pts = 0
@@ -24,24 +21,24 @@ class Resident:
 
         params = (self.previous_pts, self.room_assignment_pts, self.chores_pts, self.rex_cpw_pts, 
         self.retreat_attendance_pts, self.meeting_pts, self.total_housing_pts, self.kerb,)
-        cursor.execute("""UPDATE housing_points SET previous_points=%s, room_assignment=%s, chores=%s, 
-         REX_CPW=%s, retreat_attendance=%s, meeting_points=%s, TOTAL=%s WHERE kerb=%s""", params)
 
+        cursor.execute("""UPDATE housing_points SET previous_points=%s, room_assignment=%s, chores=%s, 
+         REX_CPW=%s, retreat_attendance=%s, meeting_points=%s, total_housing_points=%s WHERE kerb=%s""", params)
 
 def create_residents(cursor):
-    cursor.execute("""SELECT * FROM people""")
+    cursor.execute("""SELECT people.kerb, resident_info.office, resident_info.room FROM people LEFT JOIN resident_info ON people.kerb=resident_info.kerb WHERE people.resident=1""")
     rows = cursor.fetchall()
 
     residents = []
-    for fname, lname, kerb, year, position, course, room in rows:
-        onExec = position != ""
-        resident = Resident(fname, lname, kerb, year, onExec, room)
+    for kerb, position, room in rows:
+        onExec = (position != "" and position != "Webmaster")
+        resident = Resident(kerb, onExec, room)
         residents.append(resident)
     
     return residents
 
 def create_dicts(cursor):
-    cursor.execute("""SELECT kerb, TOTAL FROM housing_points""")
+    cursor.execute("""SELECT kerb, total_housing_points FROM housing_points""")
     rows = cursor.fetchall()
     prev_pts = {kerb:pts for kerb, pts in rows}
 
@@ -50,14 +47,14 @@ def create_dicts(cursor):
     gbm_attendance = {}
     retreat_attendance = {}
     REX_CPW = {}
-    for fname, lname, kerb, status1, status2, status3, status4, retreat, rex_cpw in rows:
+    for kerb, status1, status2, status3, status4, retreat, rex_cpw in rows:
         gbm_attendance.update({kerb:[status1, status2, status3, status4]})
         retreat_attendance.update({kerb:retreat})
         REX_CPW.update({kerb:rex_cpw})
 
     cursor.execute("""SELECT * FROM EBM_attendance""")
     rows = cursor.fetchall()
-    ebm_attendance = {kerb: [status1, status2, status3, status4] for fname, lname, kerb, status1, status2, status3, status4 in rows}
+    ebm_attendance = {kerb: [status1, status2, status3, status4] for kerb, status1, status2, status3, status4 in rows}
     
     cursor.execute("""SELECT * FROM rooms""")
     rows = cursor.fetchall()
@@ -71,6 +68,7 @@ def create_dicts(cursor):
 
 def calculate_points(residents, dicts, cursor):
     prev_pts, gbm_attendance, retreat_attendance, REX_CPW, ebm_attendance, rooms, chores = dicts
+
     for r in residents:
         #previous pts
         r.previous_pts = prev_pts[r.kerb]
@@ -78,13 +76,13 @@ def calculate_points(residents, dicts, cursor):
         #rex/cpw pts
         if REX_CPW[r.kerb] == "absent":
             r.rex_cpw_pts = 0
-        else:
+        elif REX_CPW[r.kerb] == "present":
             r.rex_cpw_pts = .5
 
         #retreat pts
         if retreat_attendance[r.kerb] == "absent":
             r.retreat_attendance_pts = 0
-        else:
+        elif retreat_attendance[r.kerb] == "present":
             r.retreat_attendance_pts = .5
         
         #meeting pts
@@ -92,19 +90,25 @@ def calculate_points(residents, dicts, cursor):
         for attendance in gbm_attendance[r.kerb]:
             if attendance == "late":
                 meetings_attended += .5
-            elif attendance != "absent":
+            elif attendance == "present":
                 meetings_attended += 1
+
+        cursor.execute("""SELECT * FROM GBM_dates WHERE dates!='0000-00-00'""")
+        num_gbms = len(cursor.fetchall())
+
+        cursor.execute("""SELECT * FROM EBM_dates WHERE dates!='0000-00-00'""")
+        num_ebms = len(cursor.fetchall())
 
         if r.onExec:
             for attendance in ebm_attendance[r.kerb]: 
                 if attendance == "late":
                     meetings_attended += .5
-                elif attendance != "absent":
+                elif attendance == "present":
                     meetings_attended += 1
-            r.meeting_pts = (meetings_attended/8)
+            r.meeting_pts = (meetings_attended/(num_gbms + num_ebms))
 
         else: 
-            r.meeting_pts = (meetings_attended/4) * .5
+            r.meeting_pts = (meetings_attended/(num_gbms)) * .5
 
         #room assignment pts
         if rooms[r.room] == "SINGLE": r.room_assignment_pts = .5
@@ -115,7 +119,7 @@ def calculate_points(residents, dicts, cursor):
         # total_chores = cursor.fetchall()[0][0]
         r.chores_pts = chores[r.kerb] * .05
 
-        r.set_all_pts(cursor)
+        # r.set_all_pts(cursor)
 
 if __name__ == "__main__":
     connection = pymysql.connect(host="sql.mit.edu", user="la_casa", passwd="la_casa-webmaster", database="la_casa+site")
